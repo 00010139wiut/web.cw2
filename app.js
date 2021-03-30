01e40089fc5865c506c1ff190e76b79562223d59
00010139wiut <10139wiut@gmail.com>
2021-03-30 14:13:57 +0500

    starting layout, first route

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
+
+
+app.set('view engine', 'pug')
+app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+app.listen(8000,  err => {
+    if(err) console.log(err)
+
+    console.log('Server is running on port 8000...')
+})
