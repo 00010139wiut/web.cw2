ab33e52848c5bb12a3a97e27ffd79551b3aa212a
00010139wiut <10139wiut@gmail.com>
2021-03-30 14:25:55 +0500

    create functionality

--- app.js
+++ app.js
@@ -12,9 +12,39 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const body = req.body.body
+
+    fs.readFile('./data/todos.json', (err, data) => {
+        if (err) throw err
+
+        const todos = JSON.parse(data)
+
+        todos.push({
+            id: id(),
+            title: title,
+            body: body,
+        })
+
+        fs.writeFile('./data/todos.json', JSON.stringify(todos), err => {
+         if (err) throw err
+         res.redirect('/create')
+        })
+    })
+})
 
 app.listen(8000,  err => {
     if(err) console.log(err)
 
     console.log('Server is running on port 8000...')
 })
+
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
